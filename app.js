b2789e588f8d4b068eb296d453bbdf4f38bbfb30
Rachit Yadav <yadavrachit148@gmail.com>
2025-06-09 21:42:08 +0530

    Created Review Model

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
-const {listingSchema} = require("./schema.js");
-
+const {listingSchema, reviewSchema} = require("./schema.js");
+const Review = require("./models/review.js");
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
 
@@ -48,6 +48,18 @@ const validateListing = (req, res, next) => {
   }
 };
 
+const validateReview = (req, res, next) => {
+    let {error} = reviewSchema.validate(req.body);
+  if (error)
+  {
+    let errMsg = error.details.map((el) => el.message).join(".");
+    throw new ExpressError(400, errMsg);
+  }
+  else{
+    next();
+  }
+};
+
 app.get("/listings", wrapAsync(async (req, res) => {
   const allListings = await Listing.find({});
   res.render("listings/index.ejs", {allListings});
@@ -61,7 +73,7 @@ app.get("/listings/new", (req, res) => {
 
 app.get("/listings/:id", wrapAsync(async(req, res) => {
     let {id} = req.params;
-   const listing =  await Listing.findById(id);
+   const listing =  await Listing.findById(id).populate("reviews");
    res.render("listings/show.ejs", {listing});
 }));
 
@@ -90,6 +102,27 @@ app.delete("/listings/:id", wrapAsync(async(req, res) => {
     console.log(deletedListing);
     res.redirect("/listings");
 }));
+
+app.post("/listings/:id/reviews", validateReview, wrapAsync(async(req, res) => {
+   let listing = await Listing.findById(req.params.id);
+   let newReview = new Review(req.body.review);
+
+   listing.reviews.push(newReview);
+
+   await newReview.save();
+   await listing.save();
+
+   res.redirect(`/listings/${listing._id}`);
+}));
+
+app.delete("/listings/:id/reviews/:reviewId", wrapAsync(async(req, res) => {
+    let {id, reviewId} = req.params;
+    await Listing.findByIdAndUpdate(id, {$pull: {reviews: reviewId}});
+    await Review.findByIdAndDelete(reviewId);
+
+    res.redirect(`/listings/${id}`);
+})
+);
 
 // app.get("/testListing", async (req, res) => {
 //     let sampleListing = new Listing({
--- schema.js
+++ schema.js
@@ -2,6 +2,7 @@ const Joi = require('joi');
 
 module.exports.listingSchema = Joi.object({
     listing: Joi.object({
+    title: Joi.string().required(),
     description: Joi.string().required(),
     location: Joi.string().required(),
     country: Joi.string().required(),
@@ -10,3 +11,9 @@ module.exports.listingSchema = Joi.object({
     }).required()
 });
 
+module.exports.reviewSchema = Joi.object({
+    review: Joi.object({
+        rating: Joi.number().required().min(1).max(5),
+        comment: Joi.string(),
+    }).required(),
+});
